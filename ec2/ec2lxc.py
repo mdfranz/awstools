329f6cb35e9427fea43e0a535a575f4dbe2cd295
Matt Franz <mdfranz@gmail.com>
2012-02-05 17:39:37 -0500

    Refactoring and chef-solo run works now

--- ec2/ec2lxc.py
+++ ec2/ec2lxc.py
@@ -4,9 +4,11 @@
 #AWS_ACCESS_KEY_ID = ''
 #AWS_SECRET_ACCESS_KEY = ''
 #AWS_SSH_PEM=''
+
 AMI = "ami-e358958a"
 AMI_TYPE = "t1.micro"
 DEBUG=True
+SSH_MAXCOUNT=10
 
 import boto,time,sys,os,tempfile
 
@@ -23,8 +25,59 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get -y install chef
 mkdir -p /var/chef/cookbooks
-chef-solo -ldebug
+git clone git://github.com/mdfranz/cookbooks.git
+cp -av cookbooks/lxc /var/chef/cookbooks/
 """
+
+# Chef Node file for only LXC
+node_json="""
+{
+        "run_list": "lxc"
+}
+"""
+
+def ssh_boot(s,debug=True):
+    """Run commands to boot AMI"""
+    # Create the bootstrap file
+    boot_file = tempfile.NamedTemporaryFile()
+    boot_file.write(chef_bootscript)
+    boot_file.flush()
+
+    if debug:
+        print "Creating:\n",boot_file.name
+
+    # Create the node_json
+    node_file = tempfile.NamedTemporaryFile()
+    node_file.write(node_json)
+    node_file.flush()
+
+    if debug:
+        print "Creating:\n",node_file.name
+
+    # Get git
+    (stdin, stdout, stderr) = s.exec_command('sudo apt-get -y update; sudo apt-get -y install git-core')
+
+    if debug:
+        print stdout.readlines()
+
+    # Upload Bootstrap Chef Solo
+    sftpclient = ssh.SFTPClient.from_transport(s.get_transport())
+    sftpclient.put(boot_file.name,"/tmp/chefboot.sh")
+
+    # Install Chef
+    (stdin, stdout, stderr) = s.exec_command('sudo sh /tmp/chefboot.sh')
+
+
+    if debug:
+        print stdout.readlines()
+
+    sftpclient.put(node_file.name,"/home/ubuntu/node.json")
+
+    (stdin, stdout, stderr) = s.exec_command('sudo chef-solo -j /home/ubuntu/node.json')
+
+    if debug:
+        print stdout.readlines()
+
 
 def sho_run(e):
     """Dump out all the instances per reservation"""
@@ -44,65 +97,54 @@ if __name__ == "__main__":
         sys.exit()
 
     if sys.argv[1] == "start":
-
-        # Create new minimal AWS Linux AMI
+        # Create new minimal AWS Linux AMI (get key name)
         ssh1 = os.environ['AWS_SSH_PEM'].split('/')[-1]
         ssh2 = ssh1.split('.')[0]
 
-        # Get the reservation
+        # Get the reservations
         r = e.run_instances(AMI,key_name=ssh2,instance_type=AMI_TYPE)
-
         print "Creating instance in:",r.region.name
 
         i = r.instances[-1]
         print "Launch time:",i.launch_time
-        time.sleep(5)
 
-        # Dump all the instances
         while i.state != "running":
             i.update()
             print "State:",i.state
             time.sleep(5)
 
-        host = i.public_dns_name
-        print "Host name:",host
 
-        # Create the bootstrap file
-        boot_file = tempfile.NamedTemporaryFile()
-        boot_file.write(chef_bootscript)
-        boot_file.flush()
-        print "Creating:\n",boot_file.name
-
-        print "Waiting 30 seconds for SSH"
-        time.sleep(30)
+        print "Host name assigned:",i.public_dns_name
 
         # Create the channel
         sclient = ssh.SSHClient()
         sclient.set_missing_host_key_policy(ssh.AutoAddPolicy())
-        sclient.connect(host, username='ubuntu', key_filename=os.environ['AWS_SSH_PEM'])
-        print "Connected to: %s" % host  
 
-        # Get git
-        (stdin, stdout, stderr) = sclient.exec_command('sudo apt-get -y update; sudo apt-get -y install git-core')
-        print stdout.readlines()
+        ssh_count = 1
 
-        sftpclient = ssh.SFTPClient.from_transport(sclient.get_transport())
-        sftpclient.put(boot_file.name,"/tmp/chefboot.sh")
-        print `sftpclient.listdir('/tmp')`
+        while ssh_count < SSH_MAXCOUNT:
+            try:
+                print "Attempting to connect to: ", i.public_dns_name
+                sclient.connect(i.public_dns_name, username='ubuntu', key_filename=os.environ['AWS_SSH_PEM'])
+                break
+            except:
+                ssh_count+=1
+                wait_time = ssh_count * 2
+                print "Cannot connect to %s waiting for %d seconds" % (i.public_dns_name,wait_time)
+                time.sleep(wait_time)
 
-        # Install Chef
-        (stdin, stdout, stderr) = sclient.exec_command('sudo sh /tmp/chefboot.sh')
-        print stdout.readlines()
+        print "Connected to: %s" % i.public_dns_name
 
+        ssh_boot(sclient)
         sclient.close()
 
     elif sys.argv[1] == "stop":
         r = e.get_all_instances()
         for s in r:
             for i in s.instances:
-                print "Host name:",i.public_dns_name, i.state
-
                 if i.state != "terminated":
+                    print "Host:",i.public_dns_name, i.state
+
                     while i.state != "terminated":
                         try:
                             i.stop()
@@ -114,6 +156,5 @@ if __name__ == "__main__":
                         print "State:",i.state
                         time.sleep(5)
                         i.terminate()
-                print "State:",i.state
     else:
         sho_run(e)
