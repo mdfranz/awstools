df26f18f9084eefe3e9d74793c739593ea812e6f
Matt Franz <mdfranz@gmail.com>
2012-02-12 16:54:44 -0500

    Cool Graphviz stuff

--- ec2/ec2fw.py
+++ ec2/ec2fw.py
@@ -7,8 +7,36 @@
 
 settings = {}
 settings["debug"] = True
+settings["sanitize"] = True
 
 import boto,time,sys,os,tempfile
+
+def quotify(s):
+    return '"'+s+'"'
+
+def to_dot(sg_list):
+    pairs = []
+    flows = {}
+    for s in sg_list:
+        for r in s.rules:
+            for g in r.grants:
+                if settings['sanitize']:
+                    if g.__repr__().find('-') > -1:
+                        src = g.__repr__().split('-')[0]
+                    else:
+                        src = g.__repr__()
+
+                p = (src, s.name)
+                if p[0]:
+                    if p not in pairs:
+                        pairs.append(p)
+                        flows[p] = []
+                    flows[p].append(r.ip_protocol + "/" + r.to_port)
+
+    for f in flows.keys():
+        rules =  " ".join(flows[f])
+        label =  '[label="' + rules + '"];'
+        print quotify(f[0]) + "->" + quotify(f[1]) + label
 
 def show_sg(e):
     """Dump out all the instances per reservation"""
@@ -29,3 +57,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         show_sg(e)
         sys.exit()
+    else:
+        if sys.argv[1] == "plot":
+            to_dot(e.get_all_security_groups())
