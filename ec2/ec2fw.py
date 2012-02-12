9ce2f6e9ee313a1c50d6de1277c97b0bdc10df71
Matt Franz <mdfranz@gmail.com>
2012-02-12 13:16:41 -0500

    Lots of security group goodness

--- ec2/ec2fw.py
+++ ec2/ec2fw.py
@@ -5,19 +5,27 @@
 #AWS_SECRET_ACCESS_KEY = ''
 #AWS_SSH_PEM=''
 
+settings = {}
+settings["debug"] = True
+
 import boto,time,sys,os,tempfile
 
 def show_sg(e):
     """Dump out all the instances per reservation"""
-    print "---- Security Groups ----"
     for s in e.get_all_security_groups():
-        print "[%s]" % s.name
+        print "\n --- %s/%s ---" % (s.name,s.id)
+        print "[Instance Members]"
         for i in s.instances():
-            print " - " +  i.public_dns_name
+            print "\t%s (%s)" % (i.public_dns_name, i.private_ip_address)
+        print "[Rules]"
+        for r in s.rules:
+            print "\t SRC: %s DST: %s/%s " % (r.grants,r.ip_protocol,r.to_port)
 
 if __name__ == "__main__":
-    print "Connecting to EC2..."
+    if settings["debug"]:
+        print "Connecting to EC2..."
     e = boto.connect_ec2()
+
     if len(sys.argv) == 1:
         show_sg(e)
         sys.exit()
