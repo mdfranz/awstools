22280bc16830e6beac06837d4da0324d43619e7a
Matt Franz <matt.franz@mandiant.com>
2014-05-26 17:15:47 -0400

    A little less crappy

--- s3/s3logcat.py
+++ s3/s3logcat.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-import boto,sys,os
+import boto,sys,os,string
 
+def usage():
+  print "set LOGBUCKET to a bucket name"
+  print "Usage:"
+  print "\ts3logcat.py [prefix] [datestring]"
+  print "\t Examples: s3logcat.py logs 2013-09-29"
 
-print `sys.argv`
+if len(sys.argv) == 1:
+  usage()
+  sys.exit()
 
-if len(sys.argv) > 1:
-  prefix = ""
-else:
+if len(sys.argv) > 1 :
   prefix = sys.argv[1]
 
 if len(sys.argv) > 2:
@@ -15,19 +20,17 @@ if len(sys.argv) > 2:
 else:
   key_match = ""
 
+
+print "Prefix %s Match: %s" % (prefix,key_match)
+
 if os.environ.has_key("LOGBUCKET"):
   s3 = boto.connect_s3()
   bucket = s3.get_bucket(os.environ["LOGBUCKET"])
+  print "Using bucket: %s" % bucket.name
 
   for k in bucket.list(prefix):
-    if key_match != "":
-      if k.name.find(key_match) > -1:
-        print k.get_contents_as_string()
-    else:
-        print k.get_contents_as_string()
+    if key_match == ""  or k.name.find(key_match) > 0:
+      l =  k.get_contents_as_string().rstrip()
+      parsed = l.split()
 
-else:
-  print "set LOGBUCKET to a bucket name"
-  print "Usage:"
-  print "\ts3logcat.py [prefix] [datestring]"
-  print "\t Examples: s3logcat.py logs 2013-09-29"
+      print parsed[2]+parsed[3], parsed[4], parsed[5], parsed[7]
