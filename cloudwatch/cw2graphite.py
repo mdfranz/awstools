83b29ad70f52fe6c0c980466db1146f4cb10cc14
Matt Franz <mdfranz@gmail.com>
2014-11-29 18:48:43 -0500

    Added general list of metrics to graph

--- cloudwatch/cw2graphite.py
+++ cloudwatch/cw2graphite.py
@@ -6,7 +6,6 @@ Create these files:
 
 https://github.com/edasque/cloudwatch2graphite/blob/master/conf/metrics.json.sample
 
-
 {
     "metrics": [
             {
@@ -25,22 +24,67 @@ https://github.com/edasque/cloudwatch2graphite/blob/master/conf/metrics.json.sam
 
 import boto.ec2.cloudwatch,sys,json
 
+metric_config = {}
+metric_config["AWS/RDS"] = {
+  "dimension": "DBInstanceIdentifier",
+  "metrics": { 
+    "NetworkTransmitThroughput": ["Average","Bytes"],
+    "NetworkReceiveThroughput": ["Average","Bytes"],
+    "ReadLatency": ["Average","Seconds"],
+    "WriteLatency": ["Average","Seconds"],
+    "ReadIOPS": ["Average","Count/Second"],
+    "WriteIOPS": ["Average","Count/Second"],
+    "DatabaseConnections": ["Average","Count"],
+    "CPUUtilization": ["Average","Percent"],
+    "DiskQueueDepth": ["Average","Count"]
+  }
+}
+
+metric_config["AWS/ELB"] = {
+  "dimension": "LoadBalancerName",
+  "metrics": {
+    "SurgeLength": [],
+    "Latency": [],
+    "RequestCount": [],
+  }  
+}
+
+def dumpe_metric(m,debug=True):
+  """Extract the goodies"""
+  m_hash = {}
+  if debug:
+    print "Name:",m.name
+    print "Namespace: ",m.namespace
+    print "Dimensions:",
+  for d in m.dimensions.keys():
+    for v in m.dimensions[d]:
+      if debug:
+        print d,v
+
 c = boto.ec2.cloudwatch.connect_to_region(sys.argv[1])
+
+# This will become the metrics.json
 cwatch_config = {}
-cwatch_config["carbonNameSpacePrefix"] = "cloudwatch"
+cwatch_config["carbonNameSpacePrefix"] = "cloudwatch" + "-" + sys.argv[1]
 cwatch_config["numberOfOverlappingPoints"] = 2
 cwatch_config["metrics"] = []
 
+# Dump all the metrics for a given region
 for m in c.list_metrics():
   metric = {}
-  if m.namespace == "AWS/RDS":
-    if m.dimensions.has_key("DBInstanceIdentifier"):
-        metric["Namespace"] = m.namespace
-        metric["MetricName"] = "CPUUtilization"
-        metric["Unit"] = "Percent"
-        metric["Dimensions.member.1.Name"] = "DBInstanceIdentifier"
-        metric["Statistics.member.1"] = "Average"
-        metric["Dimensions.member.1.Value"] = m.dimensions["DBInstanceIdentifier"][0]
+
+  # Skip AWS resources we don't care about
+  if m.namespace in metric_config.keys():
+    for d in m.dimensions.keys():
+      if d == metric_config[m.namespace]["dimension"]:
+        if m.name in metric_config[m.namespace]["metrics"].keys():
+          if len(metric_config[m.namespace]["metrics"][m.name]) > 0:
+            metric["Namespace"] = m.namespace
+            metric["MetricName"] = m.name
+            metric["Dimensions.member.1.Name"] = d
+            metric["Dimensions.member.1.Value"] = m.dimensions[d][0]
+            metric["Unit"] = metric_config[m.namespace]["metrics"][m.name][0]
+            metric["Statistics.member.1"] = metric_config[m.namespace]["metrics"][m.name][1]
   if metric != {}:
     cwatch_config["metrics"].append(metric)
 
