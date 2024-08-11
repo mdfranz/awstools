62e66d2789171297b3cb799a7937669eddca2454
Matt Franz <mdfranz@gmail.com>
2024-08-11 09:37:17 -0400

    eksctl IAM policies

--- eks/get-clusters.sh
+++ eks/get-clusters.sh
@@ -7,5 +7,4 @@ do
   echo "=== $region"
   aws --region $region eks list-clusters
   aws ec2 describe-instances --region $region --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName,State.Name]' --output table
-
 done
