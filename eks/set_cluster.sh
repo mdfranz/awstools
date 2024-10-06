dc5ddb55ac4a0fb19d5f296674cd277c1adf6f94
Matt Franz <mdfranz@gmail.com>
2024-10-06 13:03:52 -0400

    error handling

--- eks/set_cluster.sh
+++ eks/set_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#
+set -e
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <region> <cluster_name>"
@@ -11,5 +11,13 @@ EKS_REGION="$1"
 EKS_CLUSTER_NAME="$2"
 
 aws eks --region $EKS_REGION update-kubeconfig --name $EKS_CLUSTER_NAME
+
+if [ $? -ne 0 ]; then
+    echo "Failed to update kubeconfig for EKS Cluster $EKS_CLUSTER_NAME in region $EKS_REGION"
+    exit 1
+fi
+
+
+
 kubectl get nodes
 kubectl cluster-info
